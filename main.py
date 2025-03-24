46ae3ac596f4ccf29c6c2fa297f2d39c26065968
Tue Nhi Tran <40323714+ellynnhitran@users.noreply.github.com>
2025-03-24 13:03:33 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.simulation import ConveyorBeltSimulation
+
+def main():
+    print("Starting Conveyor Belt Simulation...")
+    simulation = ConveyorBeltSimulation()
+    simulation.run_simulation()
+
+if __name__ == "__main__":
+    main()
