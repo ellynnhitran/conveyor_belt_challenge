d605e5d0294c4d0018b610353c15671c832a13c5
Tue Nhi Tran <40323714+ellynnhitran@users.noreply.github.com>
2025-03-24 13:03:13 +0000

    Delete Conveyor Belt directory

--- Conveyor Belt/main.py
+++
@@ -1,9 +0,0 @@
-from src.simulation import ConveyorBeltSimulation
-
-def main():
-    print("Starting Conveyor Belt Simulation...")
-    simulation = ConveyorBeltSimulation()
-    simulation.run_simulation()
-
-if __name__ == "__main__":
-    main()
